173422fe9a194afee59df44437b50c57895716a0
Lyndon White <oxinabox@ucc.asn.au>
2018-05-18 15:00:09 +0800

    =Proof of Concept

--- README.md
+++ README.md
@@ -6,3 +6,50 @@
 [![Build Status](https://ci.appveyor.com/api/projects/status/github/oxinabox/UniversalTensorBoard.jl?svg=true)](https://ci.appveyor.com/project/oxinabox/UniversalTensorBoard-jl)
 [![CodeCov](https://codecov.io/gh/oxinabox/UniversalTensorBoard.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/oxinabox/UniversalTensorBoard.jl)
 [![Coveralls](https://coveralls.io/repos/github/oxinabox/UniversalTensorBoard.jl/badge.svg?branch=master)](https://coveralls.io/github/oxinabox/UniversalTensorBoard.jl?branch=master)
+
+
+This is a proof of concept for using TensorBoard for arbitrary numeric logging.
+You could use it with Flux.jl,  or with what ever else you want.
+
+## Usage
+usage is very simple
+
+ - `@tb_log foo` to log the current value of `foo`.
+ - `set_tb_logdir(new_dir)` sets the logging directory to `new_dir`
+ - `reset_tb_logs()` clears all current logs
+
+
+## Example
+
+### Scalars
+
+```
+using UniversalTensorBoard
+
+best_ele = -1
+total = 0
+for (ii, ele) in enumerate(50rand(1_000))
+    if ele > best_ele
+        best_ele=ele
+        @tb_log best_ele
+    end
+    total+=ele
+    running_mean = total/ii
+    @tb_log running_mean
+end
+```
+
+![The values of the variables at each log call](docs/imgs/eg-scalar.png)
+
+### Vectors -> Histograms
+
+```
+using UniversalTensorBoard
+
+for ii in 0:0.2:3
+    @tb_log randn(100)+ii
+end
+```
+
+![Histogram of values taken by at each log call](docs/imgs/eg-histogram.png)
+
--- src/UniversalTensorBoard.jl
+++ src/UniversalTensorBoard.jl
@@ -1,6 +1,116 @@
-__precompile__()
+__precompile__(false)
 module UniversalTensorBoard
 
-# Package code goes here.
+using TensorFlow
 
+Summaries = TensorFlow.summary
+
+export set_tb_logdir, @tb_log, reset_tb_logs
+export tensorboard
+
+
+mutable struct TensorBoardSession
+    logdir::String
+    sess::Session
+    writer::Summaries.FileWriter
+    global_step::Int
 end
+
+function TensorBoardSession(logdir; overwrite=false)
+    if overwrite
+        rm(logdir; force=true, recursive=true)
+    end
+    mkpath(logdir)
+    
+    sess= Session(Graph())
+    writer = Summaries.FileWriter(logdir; graph=sess.graph)
+    TensorBoardSession(realpath(logdir), sess, writer, 0)    
+end
+
+
+TensorBoardSession() = TensorBoardSession("tensorboard_logs", overwrite=true)
+# normally the logs don't overwrite, but if you've not given a path, you clearly don't care.
+
+const default_logging_session = Ref(TensorBoardSession())
+
+####################################################################################################
+# Interface
+
+"""
+    tensorboard()
+
+Opens Tensorboard in the systems default browser.
+"""
+tensorboard(tb_sess=default_logging_session[]) = visualize(tb_sess.writer)
+
+
+
+"""
+    set_tb_logdir(logdir, overwrite=false)
+
+Start a new log in the given directory
+"""
+function set_tb_logdir(logdir, overwrite=false)
+    default_logging_session[] = TensorBoardSession(logdir, overwrite=overwrite)
+end
+
+"""
+    reset_tb_logs()
+
+Reset the current log, deleteing all information
+"""
+function reset_tb_logs()
+    logdir = default_logging_session[].logdir
+    default_logging_session[] = TensorBoardSession(logdir, overwrite=true)
+end
+
+
+macro tb_log(name)
+    :(_tb_log($(esc(string(name))), $(esc(name))))
+end
+
+
+#################################################################
+# Implmentation 
+function _tb_log(name, value; tb_sess = default_logging_session[])
+    placeholder_node, summary_node  = get_nodes!(tb_sess, name, value)
+    summaries = run(tb_sess.sess, summary_node,  Dict(placeholder_node=>value))
+    write(tb_sess.writer, summaries, tb_sess.global_step)
+    tb_sess.global_step+=1
+    value
+end
+
+name_fix(name)=replace(name, r"[^A-Za-z0-9.]|\s", "_")
+placeholder_node_name(name) = name_fix(name *"_input")
+summary_node_name(name) = name_fix(name)
+
+has_node(graph, name) = !isnull(TensorFlow.get_node_by_name(graph, name))
+
+function get_nodes!(tb_sess, name, value)
+    
+    gg = tb_sess.sess.graph 
+    if has_node(gg, placeholder_node_name(name)) 
+        (gg[placeholder_node_name(name)], gg[summary_node_name(name)])
+    else
+        TensorFlow.as_default(gg) do
+            create_nodes!(name, value)
+        end
+    end
+end
+
+
+function create_nodes!(name, value::T) where T<:Number
+    placeholder_node = placeholder(T; name=placeholder_node_name(name), shape=[])
+    summary_node = Summaries.scalar(name, placeholder_node; name=summary_node_name(name))
+    (placeholder_node, summary_node)
+end
+
+function create_nodes!(name, value::AbstractArray{T}) where T<:Number
+    shape = collect(size(value))
+    placeholder_node=placeholder(T; name=placeholder_node_name(name), shape=shape)
+    summary_node = Summaries.histogram(name, placeholder_node; name=summary_node_name(name))
+    (placeholder_node, summary_node)
+end
+
+
+end # modulee
